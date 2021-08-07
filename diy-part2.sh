c34a729876db3d7488e6209ec45ba7d49b22c7d7
TokenYan233 <506422462qq@gmail.com>
2021-08-08 02:51:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,4 +17,7 @@
 sed -i 's/OpenWrt/GHL-R-001/g' package/base-files/files/bin/config_generate
 
 # Modify the version number
-sed -i "s/OpenWrt /Yan-ME | Build: $(TZ=UTC-8 date "+%Y.%m.%d") /building Template: P3TERX | Source Code: Lean's OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/OpenWrt / @ Yan-ME ☯ Build: $(TZ=UTC-8 date "+%Y.%m.%d") /Building Script: P3TERX | Source Code: Lean's OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+
+# Modify the password to null
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
