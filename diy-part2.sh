eebf7935945e59171e1fb4dea0b68d3e59ff2a20
TokenYan233 <506422462qq@gmail.com>
2021-08-08 11:47:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,7 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # Modify the password to null
-sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+#sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
 # Modify hostname
 sed -i 's/OpenWrt/GHL-R-001/g' package/base-files/files/bin/config_generate
